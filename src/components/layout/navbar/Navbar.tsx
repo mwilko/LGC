e47de25717c053fb07a1cc3cb407b075d65d5895
Max Wilkinson <mjswilkinson@outlook.com>
2025-06-18 16:14:05 +0100

    making platform viewing changes

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,157 +1,90 @@
 import { useContext, useState } from 'react';
-// Router
 import { Link } from 'react-router-dom';
-// Toasts
-import { logoutToast } from 'utils/toasts';
-// Logo
 import lgcLogo from 'assets/images/lgc_logo_new_trans.png';
-// Icons
 import { ReactComponent as HamburguerIcon } from 'assets/icons/hamburguer-menu.svg';
-// import { ReactComponent as LoginIcon } from 'assets/icons/login.svg';
-// import { ReactComponent as UserIcon } from 'assets/icons/user.svg';
 import { ReactComponent as DropdownItemIcon } from 'assets/icons/arrow-right-circle.svg';
-// Components
 import { HamburguerMenuCanvas } from './components/HamburguerMenuCanvas/HamburguerMenuCanvas';
-// import { LoginModal } from './components/loginModal/LoginModal';
-// import { CartWidget } from './components/cartWidget/CartWidget';
-// import { UserInfo } from './components/userInfo/UserInfo';
-// Auth
-// import { useAuth } from 'hooks/useAuth';
-// Styles
-import './NavBar.css';
 import { LSModalContext } from 'contexts/LSModalContext';
-
+import './NavBar.css';
 
 export const NavBar = () => {
+  const [hamburguerMenuOpen, setHamburguerMenuOpen] = useState<boolean>(false);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const { openModal } = useContext(LSModalContext);
 
-    // State that represents if the hamburguer menu is open or not.
-    const [hamburguerMenuOpen, sethamburguerMenuOpen] = useState<boolean>(false);
-    const openHamburguerMenu = () => sethamburguerMenuOpen(true);
-    const closeHamburguerMenu = () => sethamburguerMenuOpen(false);
+  const toggleHamburguerMenu = () => setHamburguerMenuOpen(prev => !prev);
+  const openDropdown = () => setDropdownOpen(true);
+  const closeDropdown = () => setDropdownOpen(false);
 
-    // State that represents if the categories dropdown is open or not.
-    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
-    const openDropdown = () => setDropdownOpen(true);
-    const closeDropdown = () => setDropdownOpen(false);
+  return (
+    <>
+      <HamburguerMenuCanvas 
+        hamburguerMenuOpen={hamburguerMenuOpen} 
+        closeHamburguerMenu={() => setHamburguerMenuOpen(false)} 
+      />
+      <header className="navbar">
+        <div className="navbar-container">
+          <div className="nc-left">
+            <Link to='/'>
+              <img src={lgcLogo} alt="Lincolnshire Game Company Logo" />
+            </Link>
+          </div>
 
-    // Function openModal() from LSModalContext.
-    const { openModal } = useContext(LSModalContext);
+          <nav className={`nc-right ${hamburguerMenuOpen ? 'open' : ''}`}>
+            <Link className="navbar-link nbl-section" to='/'>Home</Link>
 
-    // State that represents if the user info modal is open or not.
-    const [userInfoOpen, setUserInfoOpen] = useState<boolean>(false);
-    const toggleUserInfo = () => setUserInfoOpen(!userInfoOpen);
-    const closeUserInfo = () => setUserInfoOpen(false);
+            <div 
+              onMouseEnter={openDropdown} 
+              onMouseLeave={closeDropdown}
+            >
+              <button className="navbar-link nbl-section nbl-cat">
+                Categories
+              </button>
 
-    /* Login/Signup */
+              <div className="transparent-div"></div>
 
-    // // Get the user from AuthContext
-    // const { user, logout } = useAuth();
+              <div 
+                className="cat-dropdown-menu" 
+                style={dropdownOpen ? { opacity: '1', pointerEvents: 'all' } : {}}
+              >
+                <ul>
+                  <li className="cdm-item" onClick={closeDropdown}>
+                    <Link to='/shop/venison' className="cdm-item-link">
+                      <DropdownItemIcon className="dd-item-icon" />
+                      Venison
+                    </Link>
+                  </li>
+                  <li className="cdm-item" onClick={closeDropdown}>
+                    <Link to='/shop/poultry' className="cdm-item-link">
+                      <DropdownItemIcon className="dd-item-icon" />
+                      Poultry
+                    </Link>
+                  </li>
+                  <li className="cdm-item" onClick={closeDropdown}>
+                    <Link to='/shop/game-birds' className="cdm-item-link">
+                      <DropdownItemIcon className="dd-item-icon" />
+                      Game Birds
+                    </Link>
+                  </li>
+                  <li className="cdm-item" onClick={closeDropdown}>
+                    <Link to='/shop/specialty' className="cdm-item-link">
+                      <DropdownItemIcon className="dd-item-icon" />
+                      Specialty Items
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-    // // logout() sets an empty user as logged user and closes de user info.
-    // const handleLogout = () => {
-    //     logout();
-    //     closeUserInfo();
-    //     logoutToast();
-    // };
-    
-    return (
-        <>
-            {/* Hamburguer Menu Canvas */}
-            <HamburguerMenuCanvas hamburguerMenuOpen={hamburguerMenuOpen} closeHamburguerMenu={closeHamburguerMenu} />
+            <Link className="navbar-link nbl-section" to='/shop/all'>Shop</Link>
+            <Link className="navbar-link nbl-section" to='/about'>About</Link>
 
-            {/* Login and Register Modal */}
-            {/* <LoginModal /> */}
-
-            {/* Navbar */}
-            <header className="navbar">
-                <div className="navbar-container">
-                    {/* Logo */}
-                    <div className="nc-left">
-                        <Link to='/'>
-                            <img src={lgcLogo} alt="Lincolnshire Game Company Logo" />
-                        </Link>
-                    </div>
-
-                    {/* Navigation */}
-                    <nav className="nc-right">
-                        <Link className="navbar-link nbl-section" to='/'> Home </Link>
-
-                        <div onMouseEnter={() => openDropdown()}
-                            onMouseLeave={() => closeDropdown()}
-                        >
-                            <button className="navbar-link nbl-section nbl-cat">
-                                Categories
-                            </button>
-
-                            <div className="transparent-div" style={dropdownOpen ? {opacity: '1', pointerEvents: 'all'} : {}}></div>
-
-                            <div className="cat-dropdown-menu"
-                                style={dropdownOpen ? {opacity: '1', pointerEvents: 'all'} : {}}
-                            >
-                                <ul>
-                                    <li className="cdm-item" onClick={() => closeDropdown()}><Link to='/shop/wagyu' className="cdm-item-link">
-                                        <DropdownItemIcon className="dd-item-icon" />
-                                        Wagyu
-                                    </Link></li>
-                                    <li className="cdm-item" onClick={() => closeDropdown()}><Link to='/shop/feedlot' className="cdm-item-link">
-                                        <DropdownItemIcon className="dd-item-icon" />
-                                        Feedlot
-                                    </Link></li>
-                                    <li className="cdm-item" onClick={() => closeDropdown()}><Link to='/shop/standard' className="cdm-item-link">
-                                        <DropdownItemIcon className="dd-item-icon" />
-                                        Standard
-                                    </Link></li>
-                                    <li className="cdm-item" onClick={() => closeDropdown()}><Link to='/shop/other' className="cdm-item-link">
-                                        <DropdownItemIcon className="dd-item-icon" />
-                                        Others
-                                    </Link></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <Link className="navbar-link nbl-section" to='/shop/all'> Shop </Link>
-
-                        {/* Hamburguer Menu */}
-                        <button 
-                            className="navbar-link hamburguer-menu" 
-                            onClick={ () => openHamburguerMenu() }
-                        >
-                           <HamburguerIcon className="hamburguer-icon" />
-                        </button>
-
-
-                        {/* If there is a logged user => show the user button
-                            Otherwise => show the login button */}
-
-                        {/* { user ?
-                                // User Button
-                                <div>
-                                    <button className="user-btn" onClick={ () => toggleUserInfo() }>
-                                        <UserIcon className="user-icon" />
-                                    </button>
-                                    <UserInfo 
-                                        isOpen={userInfoOpen} 
-                                        email={user.email} logout={handleLogout} closeUserInfo={closeUserInfo} 
-                                    />
-                                </div>
-                                :
-                                // Login Button
-                                <button 
-                                    className="navbar-link login-btn" 
-                                    onClick={ () => openModal() }
-                                >
-                                    <LoginIcon className="login-icon" />
-                                </button> } */}
-
-
-                        {/* Cart Button
-                        <Link to='/cart'>
-                            <CartWidget />
-                        </Link> */}
-                    </nav>
-                </div>
-            </header>
-        </>
-
-    )
+            <button className="navbar-link hamburguer-menu" onClick={toggleHamburguerMenu}>
+              <HamburguerIcon className="hamburguer-icon" />
+            </button>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -7,138 +7,97 @@ import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as AwardIcon } from 'assets/icons/award.svg';
 import { ReactComponent as AddCartIcon } from 'assets/icons/add-cart.svg';
 // Images
-import provoloneCheesesImg from 'assets/img/others/provolone.webp';
-import sausagesImg from 'assets/img/others/sausages.webp';
-import breadsImg from 'assets/img/others/breads.webp';
 import aboutUsImg from 'assets/images/sim_and_t.jpg';
 // Components
 import { ItemListContainer } from 'components/common/itemListContainer/ItemListContainer';
-import { RecipeListContainer } from 'components/common/recipeListContainer/RecipeListContainer';
-import { VideoListContainer } from 'components/common/videoListContainer/VideoListContainer';
 // Utils
 import { isIOS } from 'utils/isIOS';
-// Data
 // Styles
 import './Home.css';
 
-
 export const Home = () => {
-    
-    /* Loading state for loading screen. */
-    const [loadingScreen, setLoadingScreen] = useState({
-        loading: true,
-        percentage: 0
-    });
+  const [loadingScreen, setLoadingScreen] = useState({
+    loading: true,
+    percentage: 0
+  });
 
-    /* This useRef() hook creates a reference
-    to about us section. */
-    const aboutUs = useRef<HTMLElement>(null);
+  const aboutUs = useRef<HTMLElement>(null);
+  const location = useLocation();
 
-    /* Function that scrolls to about us section. */
-    const scrollToAboutUs = () => aboutUs?.current?.scrollIntoView();
+  const scrollToAboutUs = () => aboutUs?.current?.scrollIntoView({ behavior: 'smooth' });
 
-    /* The useLocation() hook returns an
-    object that contains all the url path info. */
-    const location = useLocation();
+  useEffect(() => {
+    location.hash === '#about-us'
+      ? scrollToAboutUs()
+      : window.scrollTo(0, 0);
+  }, [location]);
 
-    /* Scroll to top or to about us section
-    every time the window location changes. */
-    useEffect(() => {
-        
-        location.hash === '#about-us' ?
-            scrollToAboutUs()
-        :
-            window.scrollTo(0, 0);
-
-    }, [location])
-
-    /* Loading screen control. */
-    useEffect(() => {
-        let loadingInterval = setInterval(() => {
-            loadingScreen.percentage < 100 ? 
-                setLoadingScreen({
-                    ...loadingScreen,
-                    percentage: loadingScreen.percentage++
-                })
-            : 
-                setTimeout(() => {
-                    setLoadingScreen({
-                        ...loadingScreen,
-                        loading: false
-                    })
-                    clearInterval(loadingInterval);
-                }, 750)
-        }, 20);
-
-        setTimeout(() => {
+  useEffect(() => {
+    let loadingInterval = setInterval(() => {
+      setLoadingScreen(prev => {
+        const nextPercentage = prev.percentage + 1;
+        if (nextPercentage >= 100) {
+          clearInterval(loadingInterval);
+          setTimeout(() => {
             setLoadingScreen({
-                ...loadingScreen,
-                percentage: 0
-            })
-        }, 500);
-    }, [])
+              loading: false,
+              percentage: 100
+            });
+          }, 750);
+        }
+        return { ...prev, percentage: nextPercentage };
+      });
+    }, 20);
+    return () => clearInterval(loadingInterval);
+  }, []);
 
-    return (
-        <>  
-            {/* Loading Screen */}
-            <div style={loadingScreen.loading ? {} : {opacity: '0', pointerEvents: 'none'}} className="home-loading">
-                <div className="home-loading-content">
-                    <img src={Logo} alt="The Lincolnshire Game Company" />
-                    <span>{loadingScreen.percentage}%</span>   
-                </div>
+  return (
+    <>
+      {/* Loading Screen */}
+      <div style={loadingScreen.loading ? {} : { opacity: '0', pointerEvents: 'none' }} className="home-loading">
+        <div className="home-loading-content">
+          <img src={Logo} alt="The Lincolnshire Game Company" />
+          <span>{loadingScreen.percentage}%</span>
+        </div>
+      </div>
+
+      {/* Home */}
+      <div className="home">
+
+        {/* Hero Section */}
+        <section 
+          style={isIOS() ? { backgroundAttachment: 'initial' } : {}}
+          className="hero-section"
+        >
+          <div className="hs-greeting">
+            <h1>Lincolnshire’s Finest Wild Game</h1>
+            <Link className="button hsg-search-meats-btn" to='/shop/all'>Explore Our Produce</Link>
+          </div>
+        </section>
+
+        {/* About Us Section */}
+        <section ref={aboutUs} className="about-us-section">
+          <div className="aus-grid">
+            <img src={aboutUsImg} alt="Our Team in Lincolnshire" />
+            <div className="aus-info">
+              <h2>Our Heritage</h2>
+              <p>At The Lincolnshire Game Company, we are passionate about bringing the finest wild game and poultry from the heart of Lincolnshire. We value sustainability, traceability, and tradition in every cut we offer.</p>
+              <p>Since 2013, we’ve been a trusted name for chefs, restaurants, and families across the UK and Europe, delivering exceptional quality and service.</p>
+              <Link className="button aus-btn" to='/about-us'>Discover Our Story</Link>
             </div>
+          </div>
+        </section>
 
-            {/* Home */}
-            <div className="home">
-
-                {/* Hero Section */}
-                <section 
-                    style={
-                        isIOS() ? 
-                            {backgroundAttachment: 'initial'}   
-                        :
-                            {}
-                    }
-                    className="hero-section"
-                >
-                    <div className="hs-greeting">
-                        <h1>Lincolnshire's Finest Game Provider</h1>
-                        <Link className="button hsg-search-meats-btn" to='/shop/all'>Search Produce</Link>    
-                    </div>
-                </section>
-
-                {/* About Us Section */}
-                <section ref={aboutUs} className="about-us-section">
-                    
-                    <div className="aus-grid">
-                        <img src={aboutUsImg} alt="Meats Aside" />
-
-                        <div className="aus-info">
-                            <h2>About Us</h2>
-                            <p>At The Lincolnshire Game Company, we take pride in delivering the finest wild game meat from the heart of Lincolnshire. With a strong focus on traceability, sustainability, and traditional values, we supply premium-quality produce to customers across the UK and Europe.
-                            </p>
-                            <p>Established in 2013, we provide venison and poultry products to an proffesional standard to customers. We strive for the best quality and service for our customers.</p>
-                            <Link className="button aus-btn" to='/about-us'>Our Story</Link>
-                        </div>
-                    </div>
-                </section>
-
-                {/* Product Selections */}
-                <section className="bestsellers-section">
-
-                    <div className="bs-title">
-                        <AwardIcon className="award-icon" />
-                        <h2>Our Products</h2>
-                    </div>
-                    
-                    {/* Bestsellers Products Wrapper */}
-                    <ItemListContainer category={'venison'} limit={true} />
-                    
-                    <Link className="button bs-btn" to='/shop/all'>View All Meats</Link>
-
-                </section>
-
-            </div>
-        </>
-    )
-}
+        {/* Product Selections */}
+        <section className="bestsellers-section">
+          <div className="bs-title">
+            <AwardIcon className="award-icon" />
+            <h2>Our Signature Meats</h2>
+          </div>
+          <ItemListContainer category={'venison'} limit={true} />
+          <Link className="button bs-btn" to='/shop/all'>View All Meats</Link>
+        </section>
+      </div>
+    </>
+  );
+};
